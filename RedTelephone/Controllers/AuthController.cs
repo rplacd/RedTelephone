3a4740f4f710dcfe02aea394a6ecdadbfd7b005a
rplacd <maac.iin.saam@gmail.com>
2011-06-23 11:07:32 +0700

    AuthController.Login redirects back to page needed instead of checking for authed user itself.

--- RedTelephone/Controllers/AuthController.cs
+++ RedTelephone/Controllers/AuthController.cs
@@ -14,22 +14,14 @@ namespace RedTelephone.Controllers
             logger.Debug("AuthController.Login accessed");
             String formUsername = Request.Form["username"];
             String formPassword = Request.Form["password"];
-            String targetHash = hashCombo(formUsername, formPassword);
 
-            //if we're logged in, hack in a cookie in the response and redirect.
-            if (hashComboExists_p(formUsername, hashCombo(formUsername, formPassword))) {
-                logger.DebugFormat("AuthController.Login letting user {0} in.", formUsername);
-                HttpCookie cookie = new HttpCookie("Authentication");
-                cookie["username"] = formUsername;
-                cookie["hashcombo"] = hashCombo(formUsername, formPassword);
-                Response.SetCookie(cookie);
-                return Redirect(Request.Form["destination"]);
-            } else {
-                //otherwise we're just going back to the same old login screen, albeit with message and manually set params.
-                logger.WarnFormat("AuthController.Login failed with username {0} and password {1}",
-                    Request.Form["username"], Request.Form["password"]);
-                return LoginRequired(Request.Form["destination"], Request.Form["referer"], "That isn't a valid username and password combination.");
-            }
+            //set the cookie, redirect back.
+            logger.DebugFormat("AuthController.Login setting cookie with username {0} hashcombo {1}", formUsername, hashCombo(formUsername, formPassword));
+            HttpCookie cookie = new HttpCookie("Authentication");
+            cookie["username"] = formUsername;
+            cookie["hashcombo"] = hashCombo(formUsername, formPassword);
+            Response.SetCookie(cookie);
+            return Redirect(Request.Form["destination"]);
         }
 
         public ActionResult Logout()
