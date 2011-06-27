5b2d0ae331b904f3d3157fb7729077b89b417881
rplacd <maac.iin.saam@gmail.com>
2011-06-27 09:50:32 +0700

    Hidden toggler in master layout.

---
+++ RedTelephone/Controllers/ShowHiddenController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RedTelephone.Controllers
+{
+    public class ShowHiddenController : RedTelephoneController
+    {
+        public ActionResult Yes()
+        {
+            return sideEffectingAction(() => {
+                var foo = new HttpCookie("ShowHidden", "yes");
+                Response.SetCookie(foo);
+            });
+        }
+
+        public ActionResult No()
+        {
+            return sideEffectingAction(() => {
+                var foo = new HttpCookie("ShowHidden", "no") { Expires = DateTime.Now.AddYears(-1) };
+                Response.SetCookie(foo);
+            });
+        }
+    }
+}
