4e48c7eada4ce8588d31e2ba4b895791ea4bfa28
rplacd <maac.iin.saam@gmail.com>
2011-07-22 14:15:40 +0700

    Rewrote newestOffices and newestEmployees

--- RedTelephone/Models/ModelsDataContextExtensions.cs
+++ RedTelephone/Models/ModelsDataContextExtensions.cs
@@ -16,26 +16,24 @@ namespace RedTelephone.Models {
         //REFACTOR: why can't I just db.xs.orderbydescending(version).first?
         public IEnumerable<Office> newestOffices()
         {
-            var officesByName = Offices.GroupBy(o => o.code).ToList();
-            Dictionary<String, decimal> highestOfficeVersions = new Dictionary<string, decimal>();
-            foreach (IGrouping<String, Office> group in officesByName) {
-                highestOfficeVersions.Add(group.Key, group.Max(o => o.version));
+            var officeCodes = Offices.Select(o => new { contract = o.contractCode, company = o.companyCode, code = o.code }).Distinct();
+            List<Office> ret = new List<Office>();
+            foreach (var query in officeCodes) {
+                ret.Add(Offices.Where(e => e.contractCode == query.contract && e.companyCode == query.company && e.code == query.code)
+                            .OrderByDescending(e => e.version).FirstOrDefault());
             }
-            return officesByName.Select((IGrouping<String, Office> go) =>
-                go.FirstOrDefault(o => o.version == highestOfficeVersions[go.Key])
-            );
+            return ret;
         }
 
         public IEnumerable<Employee> newestEmployees()
         {
-            var employeesByName = Employees.GroupBy(o => o.code).ToList();
-            Dictionary<String, decimal> highestEmployeeVersions = new Dictionary<string, decimal>();
-            foreach (IGrouping<String, Employee> group in employeesByName) {
-                highestEmployeeVersions.Add(group.Key, group.Max(o => o.version));
+            var employeeCodes = Employees.Select(e => new { contract = e.contractCode, company = e.companyCode, code = e.code }).Distinct();
+            List<Employee> ret = new List<Employee>();
+            foreach (var query in employeeCodes) {
+                ret.Add(Employees.Where(e => e.contractCode == query.contract && e.companyCode == query.company && e.code == query.code)
+                            .OrderByDescending(e => e.version).FirstOrDefault());
             }
-            return employeesByName.Select((IGrouping<String, Employee> go) =>
-                go.FirstOrDefault(o => o.version == highestEmployeeVersions[go.Key])
-            );
+            return ret;
         }
     }
 
